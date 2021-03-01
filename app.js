2e0eb140bf76b09e6415cf5fffc38fb75e251296
AsyaCemeniel <asyaorlov89@gmail.com>
2021-03-01 17:15:26 +0200

    Add toggle for Burger

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
+const mouse = document.querySelector(".cursor");
+const mouseTxt = mouse.querySelector("span");
+const burger = document.querySelector(".burger");
+
 let controller;
 let slideScene;
 let pageScene;
-let mouse = document.querySelector(".cursor");
-let mouseTxt = mouse.querySelector("span");
 
-//--------------------Event listeners
+//--------------------Event listeners-----------------------
+
 window.addEventListener("mousemove", cursor);
 window.addEventListener("mouseover", activeCursor);
+burger.addEventListener("click", navToggle);
+
+//-------------------------Functions------------------------
 
 function animateSlides() {
   //Init Controller
@@ -86,6 +92,24 @@ function activeCursor(event) {
     mouse.classList.remove("explore-active");
     mouseTxt.innerText = "";
     gsap.to(".title-swipe", 1, { y: "100%" });
+  }
+}
+
+function navToggle(event) {
+  if (!event.target.classList.contains("active")) {
+    event.target.classList.add("active");
+    gsap.to(".line1", 0.5, { rotate: "45", y: 5, background: "black" });
+    gsap.to(".line2", 0.5, { rotate: "-45", y: -5, background: "black" });
+    gsap.to("#logo", 1, { color: "black" });
+    gsap.to(".nav-bar", 1, { clipPath: "circle(2500px at 100% -10%)" });
+    document.body.classList.add("hide");
+  } else {
+    event.target.classList.remove("active");
+    gsap.to(".line1", 0.5, { rotate: "0", y: 0, background: "white" });
+    gsap.to(".line2", 0.5, { rotate: "0", y: 0, background: "white" });
+    gsap.to("#logo", 1, { color: "white" });
+    gsap.to(".nav-bar", 1, { clipPath: "circle(50px at 100% -10%)" });
+    document.body.classList.remove("hide");
   }
 }
 
